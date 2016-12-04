7fbb44055d9d27440ddd9b900b976ca9ba8d5140
Eurybus <wraitholyth@gmail.com>
2016-12-04 18:19:46 +0200

    POST user data updating now works. YAY!

--- ProductsApp/Controllers/InputController.cs
+++ ProductsApp/Controllers/InputController.cs
@@ -44,8 +44,9 @@ namespace ProductsApp.Controllers
             MySqlCommand nameCheckCmd = new MySqlCommand(nameCheckQuery, connect);
             nameCheckCmd.Parameters.AddWithValue("@nick", user.nick);
 
-            string updateQuery = "UPDATE " + DATABASE + "." + TABLE + " SET nick = @nick, fname = @fname, lname = @lname, sex = @sex, age = @age, pic_href = @pic_href, url = @url, bio = @bio, venue_key = @venue, password_hash = @password, salt = @salt;";
-            MySqlCommand updateCmd = new MySqlCommand(updateQuery, connect);
+            string updateQuery = "UPDATE " + DATABASE + "." + TABLE + " SET pic_href = @pic_href, url = @url, bio = @bio, password_hash = @password, salt = @salt WHERE nick = @nick;";
+            string venueUpdateQuery = "SET foreign_key_checks = 0; UPDATE " + DATABASE + "." + TABLE + " SET venue_key = @venue WHERE nick = @nick; SET foreign_key_checks = 1";
+            MySqlCommand updateCmd = new MySqlCommand(updateQuery + venueUpdateQuery, connect);
             
 
             string insertQuery = "INSERT INTO dionys.users(nick,fname,lname,sex,age,pic_href,url,bio,venue_key,password_hash,salt)";
@@ -64,16 +65,18 @@ namespace ProductsApp.Controllers
                 reader.Close();
                 if(id != 0)
                 {
-                    updateCmd.Parameters.AddWithValue("@nick", user.nick);
-                    updateCmd.Parameters.AddWithValue("@fname", user.fname);
-                    updateCmd.Parameters.AddWithValue("@lname", user.lname);
-                    updateCmd.Parameters.AddWithValue("@sex", user.sex);
-                    updateCmd.Parameters.AddWithValue("@age", user.age);
-                    updateCmd.Parameters.AddWithValue("@pic_href", user.avatar);
-                    updateCmd.Parameters.AddWithValue("@url", user.url);
-                    updateCmd.Parameters.AddWithValue("@bio", user.bio);
-                    updateCmd.Parameters.AddWithValue("@venue", user.venue);
-                    updateCmd.Parameters.AddWithValue("@password", user.password);
+                    User inputUser = checkProperties(user);
+
+                    updateCmd.Parameters.AddWithValue("@nick", inputUser.nick);
+                    //updateCmd.Parameters.AddWithValue("@fname", user.fname);
+                    //updateCmd.Parameters.AddWithValue("@lname", user.lname);
+                    //updateCmd.Parameters.AddWithValue("@sex", user.sex);
+                    //updateCmd.Parameters.AddWithValue("@age", user.age);
+                    updateCmd.Parameters.AddWithValue("@pic_href", inputUser.avatar);
+                    updateCmd.Parameters.AddWithValue("@url", inputUser.url);
+                    updateCmd.Parameters.AddWithValue("@bio", inputUser.bio);
+                    updateCmd.Parameters.AddWithValue("@venue", inputUser.venue);
+                    updateCmd.Parameters.AddWithValue("@password", inputUser.password);
                     updateCmd.Parameters.AddWithValue("@salt", user.salt);
 
                     updateCmd.ExecuteNonQuery();
@@ -81,7 +84,7 @@ namespace ProductsApp.Controllers
                 }
                 else
                 {
-                    // User was not found in database, so let's create it
+                    // User was not found in database, so let's create a new user
                     insertCmd.Parameters.AddWithValue("@nick", user.nick);
                     insertCmd.Parameters.AddWithValue("@fname", user.fname);
                     insertCmd.Parameters.AddWithValue("@lname", user.lname);
@@ -100,7 +103,7 @@ namespace ProductsApp.Controllers
             }
             catch (MySqlException ex)
             {
-
+                // Modifying FK value (venue_key fails)
                 throw ex;
             }
 
@@ -132,9 +135,86 @@ namespace ProductsApp.Controllers
 
         private User checkProperties(User user)
         {
+            MySqlConnection connect = new MySqlConnection(ConnStr);
+            string nameCheckQuery = "SELECT * FROM " + DATABASE + "." + TABLE + " WHERE nick = @nick;";
+            string idCheckQuery = "SELECT * FROM " + DATABASE + "." + TABLE + "WHERE id = @id;";
+
+            string cmdQuery = "";
+            bool usingID;
+            if (user.nick == null)
+            {
+                cmdQuery = idCheckQuery;
+                usingID = true;
+            }
+            else
+            {
+                cmdQuery = nameCheckQuery;
+                usingID = false;
+            }
+            MySqlCommand cmd = new MySqlCommand(cmdQuery, connect);
+            if (usingID)
+                cmd.Parameters.AddWithValue("@id", user.Id);
+            else
+                cmd.Parameters.AddWithValue("@nick", user.nick);
+            User referenceUser = new User();
+
+            try
+            {
+                MySqlDataReader reader;
+                connect.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    referenceUser.Id = reader.GetInt32(0);
+                    referenceUser.nick = reader.GetString(1);
+                    //referenceUser.fname = reader.GetString(2);
+                    //referenceUser.lname = reader.GetString(3);
+                    //referenceUser.sex = reader.GetBoolean(4);
+                    //referenceUser.age = reader.GetInt32(5);
+                    referenceUser.avatar = reader.GetString(6);
+                    referenceUser.url = reader.GetString(7);
+                    referenceUser.bio = reader.GetString(8);
+                    /*if (reader.GetInt32(9) != 0)
+                    {
+                        referenceUser.venue = reader.GetInt32(9);
+
+                    }*/
+                    referenceUser.password = reader.GetString(10);
+                    referenceUser.salt = reader.GetString(11);
+                }
+                reader.Close();
+                connect.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
             User output = user;
-            if (output.nick == null)
-                output.nick = "";
+            /*if (output.nick == null)
+                output.nick = referenceUser.nick;*/
+            if (output.fname == null)
+                output.fname = referenceUser.fname;
+            if (output.lname == null)
+                output.lname = referenceUser.lname;
+
+            output.sex = referenceUser.sex;
+
+            output.age = referenceUser.age;
+
+            if (output.avatar == null)
+                output.avatar = referenceUser.avatar;
+            if (output.url == null)
+                output.url = referenceUser.url;
+            if (output.bio == null)
+                output.bio = referenceUser.bio;
+
+            if (output.password == null)
+                output.password = referenceUser.password;
+            if (output.salt == null)
+                output.salt = referenceUser.salt;
+
             return output;
 
         }
